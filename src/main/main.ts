d748ece6c18a1e7f8518992bb2cb55be2353d1d1
Rémi Jarasson <remi.jarasson@gmail.com>
2023-04-24 07:22:13 +0200

    fix: clicking Task icon opens app

--- src/main/main.ts
+++ src/main/main.ts
@@ -157,8 +157,7 @@ const createTray = () => {
     },
   ]);
 
-  tray.on('click', () => tray?.popUpContextMenu());
-
+  tray.on('click', () => openWindow());
   tray.setContextMenu(contextMenu);
   tray.setToolTip('MIDI Jar');
 };
