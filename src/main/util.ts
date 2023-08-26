ad707f35f8820b6013351ce1a882b32d1ebfc4a5
Rémi Jarasson <remi.jarasson@gmail.com>
2023-08-26 09:16:28 +0200

    fix: devtools & startup settings on mac

--- src/main/util.ts
+++ src/main/util.ts
@@ -1,11 +1,18 @@
-/* eslint import/prefer-default-export: off */
 import path from 'path';
+import { app } from 'electron';
+
+const RESOURCES_PATH = app.isPackaged
+  ? path.join(process.resourcesPath, 'assets')
+  : path.join(__dirname, '../../assets');
 
 export function resolveHtmlPath(htmlFileName: string) {
   if (process.env.NODE_ENV === 'development') {
     const port = process.env.PORT || 1212;
     return `http://localhost:${port}/${htmlFileName}`;
-  } else {
-    return `file://${path.resolve(__dirname, '../', htmlFileName)}`;
-  };
+  }
+  return `file://${path.resolve(__dirname, '../', htmlFileName)}`;
 }
+
+export const getAssetPath = (...paths: string[]): string => {
+  return path.join(RESOURCES_PATH, ...paths);
+};
