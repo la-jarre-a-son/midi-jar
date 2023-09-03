ce7560069f280a891ef437ce6e4943cc462d311c
Rémi Jarasson <remi.jarasson@gmail.com>
2023-09-03 15:12:11 +0200

    chore: replace ts-jest by babel-jest

---
+++ babel.config.js
@@ -0,0 +1,11 @@
+module.exports = (api) => {
+  api.cache(true);
+
+  return {
+    presets: [
+      ['@babel/preset-env', { targets: { node: 'current' } }],
+      '@babel/preset-typescript',
+      ['@babel/preset-react', { runtime: 'automatic' }],
+    ],
+  };
+};
