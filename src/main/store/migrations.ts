deaaca7e796876cba9a526f938d4489eacaf2ae2
Rémi Jarasson <remi.jarasson@gmail.com>
2023-11-07 04:50:32 +0100

    feat: migrate settings to v1.5.0

--- src/main/store/migrations.ts
+++ src/main/store/migrations.ts
@@ -1,86 +1,17 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
+/* eslint-disable @typescript-eslint/ban-ts-comment, camelcase */
 import { Migrations } from 'conf/dist/source/types';
 import Conf from 'conf';
-import {
-  ChordQuizSettings,
-  CircleOfFifthsSettings,
-  NotationSettings,
-  ServerSettings,
-} from 'main/types';
-import { MidiRouteRaw } from '../midi/MidiRoute';
 import { StoreType } from '../types/Store';
-
-type v1_1_0_ChordDisplaySettings = {
-  useInternal: boolean;
-  skin: 'classic' | 'flat';
-  from: string;
-  to: string;
-  key: string;
-  accidentals: 'flat' | 'sharp';
-  displayKeyboard: boolean;
-  displayNotes: boolean;
-  displayChord: boolean;
-  displayNotation: boolean;
-  displayAltChords: boolean;
-  displayTonic: boolean;
-  displayDegrees: boolean;
-  displayKeyNames: boolean;
-  colorHighlight: string | null;
-  colorNoteWhite: string | null;
-  colorNoteBlack: string | null;
-};
-
-type v1_1_0_Settings = {
-  chordDisplay: {
-    internal: v1_1_0_ChordDisplaySettings;
-    overlay: v1_1_0_ChordDisplaySettings;
-  };
-  general: {
-    launchAtStartup: boolean;
-    startMinimized: boolean;
-  };
-  notation: NotationSettings;
-  server: ServerSettings;
-};
-
-type v1_2_0_ChordDisplaySettings = {
-  useInternal: boolean;
-  skin: 'classic' | 'flat';
-  from: string;
-  to: string;
-  displayKeyboard: boolean;
-  displayNotes: boolean;
-  displayChord: boolean;
-  displayNotation: boolean;
-  displayAltChords: boolean;
-  displayTonic: boolean;
-  displayDegrees: boolean;
-  displayKeyNames: boolean;
-  colorHighlight: string | null;
-  colorNoteWhite: string | null;
-  colorNoteBlack: string | null;
-};
-
-type v1_2_0_Settings = {
-  chordDisplay: {
-    internal: v1_2_0_ChordDisplaySettings;
-    overlay: v1_2_0_ChordDisplaySettings;
-  };
-  circleOfFifths: CircleOfFifthsSettings;
-  notation: NotationSettings;
-  server: ServerSettings;
-};
-
-type v1_3_0_Settings = v1_2_0_Settings & {
-  chordQuiz: ChordQuizSettings;
-};
-
-type v1_3_0_MidiRouteRaw = {
-  input: string;
-  output: string;
-  type: 'physical' | 'internal' | 'websocket';
-  enabled: boolean;
-};
+import {
+  v1_0_0_MidiRouteRaw,
+  v1_1_0_Settings,
+  v1_2_0_ChordDisplaySettings,
+  v1_2_0_Settings,
+  v1_3_0_Settings,
+  v1_3_1_MidiRouteRaw,
+  v1_4_0_Settings,
+  v1_5_0_Settings,
+} from './legacy-types';
 
 const migrations: Migrations<StoreType> = {
   '1.0.0': (store: Conf<StoreType>) => {
@@ -137,15 +68,16 @@ const migrations: Migrations<StoreType> = {
     delete settings.chordDisplay.internal.accidentals;
 
     const newSettings: v1_2_0_Settings = {
+      ...settings,
+      chordDisplay: {
+        internal: settings.chordDisplay.internal,
+        overlay: settings.chordDisplay.overlay,
+      },
       notation: {
         key: settings.chordDisplay.internal.key || 'C',
         accidentals: settings.chordDisplay.internal.accidentals || 'flat',
         staffClef: 'both',
         staffTranspose: 0,
-      },
-      chordDisplay: {
-        internal: settings.chordDisplay.internal,
-        overlay: settings.chordDisplay.overlay,
       },
       circleOfFifths: {
         scale: 'major' as const,
@@ -161,20 +93,29 @@ const migrations: Migrations<StoreType> = {
         displayDegrees: false,
         displayDegreeLabels: false,
       },
-      server: settings.server,
     };
 
     store.set('settings', newSettings);
   },
   '1.3.0': (store: Conf<StoreType>) => {
     store.set('version', '1.3.0');
-    store.set('settings.chordDisplay.internal.displayIntervals', false);
-    store.set('settings.chordDisplay.overlay.displayIntervals', false);
 
     const settings = store.get('settings') as unknown as v1_2_0_Settings;
 
     const newSettings: v1_3_0_Settings = {
       ...settings,
+      chordDisplay: {
+        internal: {
+          ...settings.chordDisplay.internal,
+          displayIntervals: false,
+        },
+        overlay: settings.chordDisplay.overlay
+          ? {
+              ...settings.chordDisplay.overlay,
+              displayIntervals: false,
+            }
+          : undefined,
+      },
       chordQuiz: {
         mode: 'random' as const,
         difficulty: 0,
@@ -188,9 +129,11 @@ const migrations: Migrations<StoreType> = {
     store.set('settings', newSettings);
   },
   '1.3.1': (store: Conf<StoreType>) => {
-    const routes = store.get('midi.routes', []) as unknown as v1_3_0_MidiRouteRaw[];
+    store.set('version', '1.3.1');
 
-    const newRoutes = routes.map((r: v1_3_0_MidiRouteRaw): MidiRouteRaw => {
+    const routes = store.get('midi.routes', []) as unknown as v1_0_0_MidiRouteRaw[];
+
+    const newRoutes = routes.map((r: v1_0_0_MidiRouteRaw): v1_3_1_MidiRouteRaw => {
       if (r.type === 'websocket' && r.output === 'chord-display') {
         return { ...r, type: 'internal', output: 'chord-display/overlay' };
       }
@@ -198,14 +141,14 @@ const migrations: Migrations<StoreType> = {
         return { ...r, type: 'internal', output: 'chord-display/internal' };
       }
 
-      return r as MidiRouteRaw;
+      return r as v1_3_1_MidiRouteRaw;
     });
-
-    store.set('version', '1.3.1');
 
     store.set('midi.routes', newRoutes);
   },
   '1.4.0': (store: Conf<StoreType>) => {
+    store.set('version', '1.4.0');
+
     const oldChordDisplaySettings = store.get('settings.chordDisplay', undefined) as unknown as
       | undefined
       | {
@@ -251,6 +194,30 @@ const migrations: Migrations<StoreType> = {
         },
       ]);
     }
+  },
+  '1.5.0': (store: Conf<StoreType>) => {
+    store.set('version', '1.5.0');
+    store.set('windowState.changelogDismissed', false);
+
+    const settings = store.get('settings') as unknown as v1_4_0_Settings;
+
+    const newSettings: v1_5_0_Settings = {
+      ...settings,
+      chordDisplay: settings.chordDisplay.map((chordDisplaySettings) => ({
+        ...chordDisplaySettings,
+        chordNotation: 'short',
+        allowOmissions: true,
+        highlightAlterations: false,
+        displayName: false,
+      })),
+      chordQuiz: {
+        ...settings.chordQuiz,
+        chordNotation: 'short',
+        displayName: true,
+      },
+    };
+
+    store.set('settings', newSettings);
   },
 };
 
